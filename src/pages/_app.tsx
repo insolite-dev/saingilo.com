934ed608d9acb261a4f21315c97ad0010c082fae
Ismael Sh <me@theiskaa.com>
2023-10-14 00:54:22 +0400

    feat: add background image & text

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "@/styles/globals.css"
+import Bg from "../../public/bg.jpg"
+import Image from "next/image"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import type { AppProps } from "next/app"
@@ -7,9 +9,32 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <div>
-                <header>
-                    <Header />
-                </header>
+                <div className="relative h-screen">
+                    <Image
+                        priority
+                        src={Bg}
+                        alt="ქურმუხი, ქართული ეკლესია აზ. რ. კახის რაიონში"
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="top"
+                    />
+                    <div className="absolute left-8 bottom-10 text-white">
+                        <p className="text-4xl font-extrabold" style={{ lineHeight: '45px' }}>
+                            ჩვენ ვართ ეროვნების <br />
+                            დავიწყების კარზე მიმდგარი <br />
+                            ქართველები! <br />
+                            და ეს ჩვენი ვებ გვერდია, <br />
+                            სადაც ვცდილობთ <br />
+                            რაც შეგვინარჩუნეს <br />
+                            გავაგრძელოთ ... <br />
+                        </p>
+                    </div>
+                </div>
+                <div className="absolute top-0 left-0 right-0 bg-black/50 backdrop-blur-md">
+                    <header>
+                        <Header />
+                    </header>
+                </div>
                 <main>
                     <Component {...pageProps} />
                 </main>
