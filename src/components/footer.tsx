82265ae80e47b3ee7d00979f2fe7373e1651a737
Ismael Sh <me@theiskaa.com>
2023-10-13 22:17:04 +0400

    feat: add header and footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,48 @@
+
+import Image from "next/image"
+import Link from "next/link"
+import Logo from "../../public/saingilo_logo.svg"
+
+const navigation = [
+    { name: "Instagram", href: "https://instagram.com/saingilo_com" },
+    { name: "Facebook", href: "https://facebook.com/saingilo_com" },
+    { name: "Twitter", href: "https://twitter.com/saingilo_com" },
+]
+
+const Footer = () => {
+    return (
+        <div className="bg-[#111111] flex flex-col sm:flex-row items-center space-y-2 justify-start mx-6 py-2">
+            <div className="flex flex-col sm:flex-row flex-shrink-0 items-center">
+                <Image
+                    priority
+                    src={Logo}
+                    alt="saingilo.com"
+                    height={60}
+                    className="block h-8 w-auto lg:hidden"
+                />
+                <Image
+                    priority
+                    src={Logo}
+                    alt="saingilo.com"
+                    height={60}
+                    className="hidden h-8 w-auto lg:block"
+                />
+
+                <p className="text-white mx-4 font-light"> © 2022 Insolite, Inc. All rights reserved.</p>
+                <nav>
+                    <ul>
+                        <li className="space-x-3 my-2 sm:my-0 sm:space-x-4">
+                            {navigation.map(item => (
+                                <Link className="text-gray-400 hover:text-white" key={item.name} href={item.href}>
+                                    {item.name}
+                                </Link>
+                            ))}
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css"
+import Header from "../components/header"
+import Footer from "../components/footer"
+import type { AppProps } from "next/app"
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <div>
+                <header>
+                    <Header />
+                </header>
+                <main>
+                    <Component {...pageProps} />
+                </main>
+
+                <footer className="bg-[#111111] py-6">
+                    <Footer />
+                </footer>
+            </div>
+        </>
+    )
+}
