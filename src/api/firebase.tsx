342aaab822587d205c9f5eee24a9a69a1cbae6c6
Ismael Sh <me@theiskaa.com>
2023-10-15 16:19:08 +0400

    feat(bg): add dynamically changing background image logic

--- src/api/firebase.tsx
+++ src/api/firebase.tsx
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.APPID,
-    measurementId: process.env.MEASUREMENTID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app =
--- src/components/bg.tsx
+++ src/components/bg.tsx
@@ -6,6 +6,7 @@ import { db } from "../api/firebase";
 export default function Bg() {
     const [bgSrc, setBgSrc] = useState('');
 
+    const defaultImage = "https://firebasestorage.googleapis.com/v0/b/saingilo-com.appspot.com/o/bg1.png?alt=media&token=07dcf4e7-9b28-4b04-a6a5-2cccc7afc48d";
     useEffect(() => {
         const mainCol = collection(db, 'main');
         const initialDocRef = doc(mainCol, 'initial');
@@ -13,13 +14,25 @@ export default function Bg() {
         const fetchData = async () => {
             try {
                 const docSnapshot = await getDoc(initialDocRef);
-                if (docSnapshot.exists()) {
-                    const bg = docSnapshot.data().bg;
-                    setBgSrc(bg);
+                if (!docSnapshot.exists()) {
+                    console.error('No data found in Firebase');
+                    setBgSrc(defaultImage);
+                } else {
+                    const bgs = docSnapshot.data().bgs;
+                    if (bgs && bgs.length > 0) {
+                        const randomIndex = Math.floor(Math.random() * bgs.length);
+                        console.log("RANDOM INDEX IS: ", randomIndex);
+                        console.log("BGS", bgs);
+                        const randomBg = bgs[randomIndex];
+                        setBgSrc(randomBg);
+                    } else {
+                        console.error('No backgrounds found in Firebase');
+                        setBgSrc(defaultImage);
+                    }
                 }
             } catch (error) {
                 console.error('Error fetching data from Firebase: ', error);
-                setBgSrc("https://firebasestorage.googleapis.com/v0/b/saingilo-com.appspot.com/o/bg1.jpeg?alt=media&token=ec04f418-c92d-485a-a093-5bda1381713b&_gl=1*54ha3l*_ga*NDMzMDE5MjQyLjE2ODE5ODc4MDA.*_ga_CW55HF8NVT*MTY5NzI4MTU1NC41My4xLjE2OTcyODc4ODAuMy4wLjA.");
+                setBgSrc(defaultImage);
             }
         };
 
@@ -28,14 +41,17 @@ export default function Bg() {
 
     return (
         <div style={{ position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden' }}>
-            <Image
-                priority
-                src={bgSrc}
-                alt="ქურმუხი, ქართული ეკლესია აზ. რ. კახის რაიონში"
-                layout="fill"
-                objectFit="cover"
-                objectPosition="top"
-            />
+            {bgSrc && (
+                <Image
+                    priority
+                    src={bgSrc}
+                    alt="ქურმუხი, ქართული ეკლესია აზ. რ. კახის რაიონში"
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="top"
+                />
+            )}
         </div>
     );
 }
+
