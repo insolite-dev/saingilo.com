ebe216fb6b860cbd3ab7f9cbd24e01ea43f111fc
Ismael Sh <me@theiskaa.com>
2024-02-11 05:53:41 +0400

    feat(news): improve the head of news item page

--- src/pages/news/[id].tsx
+++ src/pages/news/[id].tsx
@@ -3,6 +3,7 @@ import { DocumentReference, collection, doc } from "firebase/firestore";
 import { getData } from '@/lib/fetcher';
 import { NewsItem } from '@/lib/types';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 export const getServerSideProps = async (context: any) => {
     const id = context.params?.id as string;
@@ -30,18 +31,30 @@ export default function NewsDetailPage({ newsItem }: { newsItem: NewsItem }) {
     }
 
     return (
-        <div className="pg bg-black flex flex-col items-center text-white">
-            <div className="flex flex-col items-center w-full md:w-3/4 xl:w-1/2">
-                <div className='p-5'>
-                    <img src={newsItem.header} alt={newsItem.title[locale]} className="mb-4 w-full rounded-lg" />
+        <>
+            <Head>
+                <title>{newsItem.title[locale]}</title>
+                <meta
+                    name="description"
+                    content={newsItem.content[locale].substring(0, 100)}
+                />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta property="og:image" content={newsItem.header} />
+                <link rel="icon" href="/logovazi.svg" />
+            </Head>
+            <div className="pg bg-black flex flex-col items-center text-white">
+                <div className="flex flex-col items-center w-full md:w-3/4 xl:w-1/2">
+                    <div className='p-5'>
+                        <img src={newsItem.header} alt={newsItem.title[locale]} className="mb-4 w-full rounded-lg" />
+                    </div>
+                    <h1 className="text-2xl md:text-3xl font-bold text-center px-4 md:px-6 pb-5">{newsItem.title[locale]}</h1>
                 </div>
-                <h1 className="text-2xl md:text-3xl font-bold text-center px-4 md:px-6 pb-5">{newsItem.title[locale]}</h1>
+                <div className="divider-no-border w-1/2"></div>
+                <p className="text-lg mx-4 mt-2 p-3 md:p-6 md:text-xl text-center w-full md:w-3/4 xl:w-1/2 text-white opacity-90">{newsItem.content[locale]}</p>
+                <div className="mt-4 mb-2 text-center w-full md:w-3/4 xl:w-1/2 text-lg text-white opacity-70">
+                    <span>{parseTimestampToDateString(newsItem.created_at.toString())}</span> - <span>{newsItem.author}</span>
+                </div>
             </div>
-            <div className="divider-no-border w-1/2"></div>
-            <p className="text-lg mx-4 mt-2 p-3 md:p-6 md:text-xl text-center w-full md:w-3/4 xl:w-1/2 text-white opacity-90">{newsItem.content[locale]}</p>
-            <div className="mt-4 mb-2 text-center w-full md:w-3/4 xl:w-1/2 text-lg text-white opacity-70">
-                <span>{parseTimestampToDateString(newsItem.created_at.toString())}</span> - <span>{newsItem.author}</span>
-            </div>
-        </div>
+        </>
     );
 }
